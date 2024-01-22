a3d0ddc449dace50f473f28f8285d3a4f5c46381
Anurag Shukla <aks76635@gmail.com>
2024-01-22 11:41:45 +0530

    basic setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import HomePage from "./component/HomePage";
+
+function App() {
+  return (
+    <section className="container">
+      <HomePage />
+    </section>
+  );
+}
+
+export default App;
